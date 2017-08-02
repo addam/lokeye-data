85c834c7ea261a3e5d0f7c7a186752e10f76603e
Adam Dominec <a.dominec@gmail.com>
2017-08-02 11:44:51 +0200

    measurements_covariance: output quartiles

--- measurements_covariance.py
+++ measurements_covariance.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 import numpy as np
 from math import hypot
+from collections import defaultdict
 mean_algo = np.zeros(7)
 mean_aspect = np.zeros(10)
 covariance = np.zeros((mean_algo.size, mean_aspect.size))
 
 n = 0
-aspect = None
 algo_map = dict()
+algo_list = defaultdict(list)
+enabled = False
+dirname = "instagram/"
 while 1:
 	try:
 		row = input().split(",")
@@ -20,9 +23,10 @@ while 1:
 		value = hypot(*(float(v) for v in row[1:])) #/ radius
 		#print("got algo:", name, "->", index, value)
 		algo[index] += value
+		algo_list[name].append(value)
 	else:
 		#add d_algo * d_aspect.t() to the covariance
-		if aspect is not None:
+		if enabled and aspect is not None:
 			diff_algo, diff_aspect = algo - mean_algo, aspect - mean_aspect;
 			mean_algo += diff_algo / (n + 1);
 			mean_aspect += diff_aspect / (n + 1);
@@ -31,13 +35,26 @@ while 1:
 		if not row:
 			#print("empty row, exit.")
 			break
+		img_name = row[0]
+		enabled = img_name.startswith(dirname)
 		aspect = np.array([float(v) for v in row[3:]])
 		algo = np.zeros(7)
 		#print("got block:", aspect)
 
-print("algo mean:")
+print("algo mean over", dirname)
 for name, index in algo_map.items():
 	print(name, mean_algo[index])
+
+print()
+print("algo quartiles", dirname)
+for name, l in algo_list.items():
+	l.sort()
+	print(name, *("{:.2f}".format(v) for v in (l[len(l)//4], l[len(l)//2], l[3*len(l)//4])), sep = " & ")
+	
+print(*algo_list.keys())
+with open("performance.tab", "w") as table:
+	for i, vals in enumerate(zip(*algo_list.values())):
+		print(i, *vals, file=table, sep="\t")
 
 print()
 print("covariance:")
